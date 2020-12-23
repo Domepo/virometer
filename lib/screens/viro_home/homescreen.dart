b28a4a3e990e4ba91286444a5c5b2aff81bc7547
Domepo <dominiktb2@gmail.com>
2020-12-23 18:32:48 +0100

    created appbar

--- lib/screens/viro_home/homescreen.dart
+++ lib/screens/viro_home/homescreen.dart
@@ -3,6 +3,32 @@ import 'package:flutter/material.dart';
 class ViroApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      theme: ThemeData(fontFamily: 'Montserrat'),
+      home: Scaffold(
+        appBar: AppBar(
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.vertical(bottom: Radius.circular(20))
+              
+              
+              
+              ),
+          centerTitle: true,
+          backgroundColor: Colors.white,
+          title: Text(
+            'Virometer',
+            style: TextStyle(color: Color(0xff3A0CA3), fontSize: 25),
+          ),
+          bottom: PreferredSize(
+              child: Text("Dienstag 22. Dezember",
+                  style: TextStyle(color: Color(0xff6E6E6E), fontSize: 10)),
+              preferredSize: Size.fromHeight(-15)),
+        ),
+        body: Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
   }
 }
+//             padding: const EdgeInsets.fromLTRB(_hPad, 32.0, _hPad, 4.0),
---
+++ lib/screens/viro_home/homescreen_appbar.dart
@@ -0,0 +1,29 @@
+// import 'package:flutter/material.dart';
+
+// class ViroAppBar extends StatelessWidget {
+//   ViroAppBar();
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         appBar: AppBar(
+//           shape: RoundedRectangleBorder(
+//               borderRadius: BorderRadius.vertical(bottom: Radius.circular(20))),
+//           centerTitle: true,
+//           backgroundColor: Colors.white,
+//           title: Text(
+//             'Vriometer',
+//             style: TextStyle(color: Color(0xff3A0CA3), fontSize: 25),
+//           ),
+//           bottom: PreferredSize(
+//               child: Text("Dienstag 22. Dezember",
+//                   style: TextStyle(color: Color(0xff6E6E6E), fontSize: 10)),
+//               preferredSize: Size.fromHeight(-15)),
+//         ),
+//         body: Center(
+//           child: Text('Hello World'),
+//         ));
+//   }
+// }
+// //             padding: const EdgeInsets.fromLTRB(_hPad, 32.0, _hPad, 4.0),
+
+// mach morgen weiter
