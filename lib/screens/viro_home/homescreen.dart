cabaef34e089d7010a1a7613b54105d5245a405c
Domepo <dominiktb2@gmail.com>
2020-12-25 21:46:16 +0100

    first box

--- lib/screens/viro_home/homescreen.dart
+++ lib/screens/viro_home/homescreen.dart
@@ -1,34 +1,34 @@
 import 'package:flutter/material.dart';
+import 'homescreen_appbar.dart';
 
 class ViroApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(fontFamily: 'Montserrat'),
-      home: Scaffold(
-        appBar: AppBar(
-          shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.vertical(bottom: Radius.circular(20))
-              
-              
-              
-              ),
-          centerTitle: true,
-          backgroundColor: Colors.white,
-          title: Text(
-            'Virometer',
-            style: TextStyle(color: Color(0xff3A0CA3), fontSize: 25),
+        theme: ThemeData(fontFamily: 'Montserrat'),
+        home: Scaffold(
+          appBar: HomeAppBar.getAppBar(),
+          body: Container(
+            margin: EdgeInsets.only(left: 20, top: 10, right: 20, bottom: 50),
+            height: 150,
+            width: double.infinity,
+            decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(color: Colors.white, width: 10),
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(18),
+                    topRight: Radius.circular(18),
+                    bottomLeft: Radius.circular(18),
+                    bottomRight: Radius.circular(18)),
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.black.withOpacity(0.15),
+                      spreadRadius: 4,
+                      blurRadius: 15,
+                      offset: Offset(0, 5))
+                ]),
           ),
-          bottom: PreferredSize(
-              child: Text("Dienstag 22. Dezember",
-                  style: TextStyle(color: Color(0xff6E6E6E), fontSize: 10)),
-              preferredSize: Size.fromHeight(-15)),
-        ),
-        body: Center(
-          child: Text('Hello World'),
-        ),
-      ),
-    );
+        ));
   }
 }
 //             padding: const EdgeInsets.fromLTRB(_hPad, 32.0, _hPad, 4.0),
