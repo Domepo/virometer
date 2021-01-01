ac15d194c4e12e034dce356bed54119b388d22da
Domepo <dominiktb2@gmail.com>
2021-01-01 21:15:48 +0100

    Listed cases

--- lib/screens/viro_home/homescreen.dart
+++ lib/screens/viro_home/homescreen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 import 'homescreen_appbar.dart';
 import 'select_box/select_box.dart';
 import 'package:dio/dio.dart';
@@ -7,44 +7,68 @@ import 'package:virometer/modules/countrys/germany/corona_request.dart';
 import 'package:virometer/modules/countrys/germany/services.dart';
 import 'dart:convert';
 
-class ViroApp extends StatelessWidget {
-  void getData() async {
-    var response = await Dio().get(
-        // "https://api.jsonbin.io/b/5fe7adebd151a57b893871c9");
+class ViroApp extends StatefulWidget {
+  @override
+  _ViroAppState createState() => _ViroAppState();
+}
+
+class _ViroAppState extends State<ViroApp> {
+  Future<CovidGerStates> getData() async {
+    var client = http.Client();
+    var covidGerModel = null;
+
+    var response = await client.get(
         "https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_Landkreisdaten/FeatureServer/0/query?where=1%3D1&outFields=cases,deaths,county&returnGeometry=false&outSR=4326&f=json");
-    //  print(response.data[0]);
-    var responseBody = response.data;
-    final welcome = welcomeFromJson(responseBody);
-    Map userMap = jsonDecode(responseBody);
-
-    print(userMap["features"][1]["attributes"]["cases"]);
-
-    // final featurea = featureFromJson(responseBody);
-    // final attributes = attributesFromJson(responseBody);
-
-    // Wenn man den Vorderen Key weglässt, dann funkt alles
-    // Also muss man nur an die welcom.feature.atribute.cases dran kommen
-    // Irgendwie geht das nicht
+    try {
+      if (response.statusCode == 200) {
+        var responseBody = response.body;
+        var repsonseBodyMap = json.decode(responseBody);
+        covidGerModel = CovidGerStates.fromJson(repsonseBodyMap);
+        return covidGerModel;
+      }
+    } catch (Exception) {
+      return covidGerModel;
+    }
+    return covidGerModel;
+    //    print(userMap["features"][0]["attributes"]["deaths"]);
   }
 
-  // void initState(){
-  //     Services.getUsers().then((value){
-  //       print(value);
-  //     });
-  // }
+  Future<CovidGerStates> _covidGerStates;
+  @override
+  void initState() {
+    _covidGerStates = getData();
+    super.initState();
+  }
 
-  // final selectVirusBox = SelectBox();
   @override
   Widget build(BuildContext context) {
-    getData();
     return MaterialApp(
         theme: ThemeData(fontFamily: 'Montserrat'),
-        home: Scaffold(
+        home: new Scaffold(
             appBar: HomeAppBar.getAppBar(),
-            body: ListView(
-              children: [
-                SelectBox("Corona", "Deutschland", "3000", "33"),
-              ],
-            )));
+            body: Container(
+          child: FutureBuilder<CovidGerStates>(
+            future: _covidGerStates,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return ListView.builder(
+                    itemCount: snapshot.data.features.length,
+                    itemBuilder: (context, index) {
+                      var ttt = snapshot.data.features[index];
+                      return Container(
+                          height: 100,
+                          child: Row(
+                            children: <Widget>[
+                              Text(ttt.attributes.cases.toString())
+                            ],
+                          ));
+                    });
+              } else {
+                return Center(child: CircularProgressIndicator());
+              }
+            },
+          ),
+        // SelectBox("Corona", "Deutschland", "3", "33"),
+        )));
   }
 }
